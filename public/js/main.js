9d0254c274d7aad2ec859047ca27dcb1c273b06c
Ryan Jones <RyanJonesHTX@pm.me>
2022-11-09 23:08:22 -0600

    spacing, home link, hide button, selected class

--- public/js/main.js
+++ public/js/main.js
@@ -10,4 +10,5 @@ document.querySelector('#main').addEventListener('click', showQuote)
 function showQuote(){
     document.querySelector('.name').classList.toggle('hide')
     document.querySelector('.quote').classList.toggle('hide')
+    document.querySelector('#main').classList.toggle('hide')
 }
