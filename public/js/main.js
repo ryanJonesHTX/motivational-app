803f1685d2dbb9f548bbc7514cf01906af00dbbd
Rachel Shin <shin.rachel@gmail.com>
2022-11-11 11:33:42 -0500

    get rid of hamburger menu, contain bg image

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,4 @@
-const hambMenu = document.querySelector('.icon');
-const ul = document.querySelector('ul')
-hambMenu.addEventListener('click',()=> {
-ul.classList.toggle("hidden");
-hambMenu.classList.toggle("open")
-})
+
 
 // button functionality
 document.querySelector('#main').addEventListener('click', showQuote)
